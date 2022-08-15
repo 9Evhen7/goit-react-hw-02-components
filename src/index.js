64619daa41d04a3a91f2e990fbe00ff8e6b24e28
9Evhen7 <96841920+9Evhen7@users.noreply.github.com>
2022-08-15 20:00:08 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
-import { FApp } from 'fComponents/fApp';
+import { App } from 'components/App';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FApp />
+    <App />
   </React.StrictMode>,
 );
